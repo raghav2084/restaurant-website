488772eb6ce2b3a1f04beedaa56327982fffa7e4
raghav2084 <raghav2084@gmail.com>
2019-03-06 23:32:34 -0600

    added express middleware to serve vendors and resources folders

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+//to serve vendors folder
+app.use("/vendors", express.static('vendors'));
+//to serve resources folder
+app.use("/resources", express.static('resources'));
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/main.html'));
