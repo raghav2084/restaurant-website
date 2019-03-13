b484296d26947acdf7cf946e4e5ba15b2501d70c
raghav2084 <raghav2084@gmail.com>
2019-03-13 18:26:20 -0500

    complete website w/ mobile nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -65,4 +65,28 @@ $(document).ready(function() {
 	}, {
 		offset: '50%'
 	});
+	
+	/*Mobile Navigation
+	when you click on the nav icon:
+	1. Display the nav items 
+	2. change the icon from hamberger to cross
+	3. toggle (open close a box)
+	*/
+	$('.js--nav-icon').click(function(){
+		/*store the result of click nav item*/
+		var nav = $('.js--main-nav');
+		var icon = $('.js--nav-icon i');
+		/*#3 open & close the box*/
+		nav.slideToggle(200);
+		
+		if(icon.hasClass('ion-ios-menu')){
+			icon.addClass('ion-ios-close');
+			icon.removeClass('ion-ios-menu');
+		}else {
+			icon.addClass('ion-ios-menu');
+			icon.removeClass('ion-ios-close');
+		}
+		
+	});
+
 });
